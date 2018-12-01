86f4c4992cec525ad96bf672ef2a5399409d8518
Swen <swenmeeu@gmail.com>
2018-12-01 23:46:05 +0100

    Implemented core mechanic

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerManager _playerManager;
+
+    private void Start()
+    {
+        _playerManager.SpawnPlayer();
+    }
+}
---
+++ Game/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _spawnPoint;
+
+    [SerializeField]
+    private Player _playerPrefab;
+
+    public Player ActivePlayer { get; set; }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnPlayer();
+        }
+    }
+
+    public void SpawnPlayer()
+    {
+        if (ActivePlayer != null)
+        {
+            KillPlayer();
+        }
+
+        var newPlayer = Instantiate(_playerPrefab);
+        newPlayer.transform.position = _spawnPoint.position;
+
+        ActivePlayer = newPlayer;
+    }
+
+    public void KillPlayer()
+    {
+        if (ActivePlayer == null)
+        {
+            return;
+        }
+
+        ActivePlayer.Kill();
+        ActivePlayer = null;
+    }
+}
