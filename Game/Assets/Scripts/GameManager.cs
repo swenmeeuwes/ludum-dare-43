ca56a8ffa690328f40095dd431220a108a496c9d
Swen <swenmeeu@gmail.com>
2018-12-02 13:27:39 +0100

    Implemented restart funcitonality

--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private PlayerManager _playerManager;
+    [SerializeField]
+    private UIManager _uiManager;
 
     public PlayerManager PlayerManager {
         get { return _playerManager; }
@@ -19,11 +20,49 @@ public class GameManager : MonoBehaviour
             _playerManager = FindObjectOfType<PlayerManager>();
         }
 
+        if (_uiManager == null)
+        {
+            _uiManager = FindObjectOfType<UIManager>();
+        }
+
         _playerManager.SpawnPlayer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Restart"))
+        {
+            StartCoroutine(RestartCoroutine());
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void Win()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator RestartCoroutine()
+    {
+        _uiManager.ShowResetText(true);
+
+        var neededHoldTime = Time.time + 3;
+        while (Input.GetButton("Restart"))
+        {
+            var holdTimeLeft = neededHoldTime - Time.time;
+            _uiManager.SetResetText(Mathf.RoundToInt(holdTimeLeft).ToString());
+
+            if (holdTimeLeft <= 0)
+            {
+                Restart();
+            }
+            yield return null;
+        }
+
+        _uiManager.ShowResetText(false);
     }
 }
---
+++ Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text RestartText;
+
+    private void Awake()
+    {
+        ShowResetText(false);
+    }
+
+    public void SetResetText(string timeLeft)
+    {
+        RestartText.text = "Restarting in " + timeLeft;
+    }
+
+    public void ShowResetText(bool show)
+    {
+        RestartText.gameObject.SetActive(show);
+    }
+}
