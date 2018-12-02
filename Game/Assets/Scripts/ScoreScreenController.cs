cfaab192c15d23027516f2b13365794bf0d04042
Swen <swenmeeu@gmail.com>
2018-12-02 23:14:22 +0100

    Implemented score screen

---
+++ Game/Assets/Scripts/ScoreScreenController.cs
@@ -0,0 +1,62 @@
+﻿using DG.Tweening;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreScreenController : MonoBehaviour {
+    public TMP_Text CompletionTimeText;
+    public TMP_Text BodiesSacrificedText;
+    public TMP_Text ContinueText;
+    public Image FadeImage;
+
+    private float _completionTimeInSeconds;
+    private bool _canContinue;
+
+    private void Awake()
+    {
+        CompletionTimeText.DOFade(0, 0);
+        BodiesSacrificedText.DOFade(0, 0);
+        ContinueText.DOFade(0, 0);
+
+        _completionTimeInSeconds = Time.time - StatsManager.Instance.StartTime;
+    }
+
+    private void Start()
+    {
+        CompletionTimeText.text = "Completed in " + (_completionTimeInSeconds / 60f).ToString("0.00") + " minutes";
+        BodiesSacrificedText.text = "Sacrificed " + StatsManager.Instance.BodiesLost  + " bodies";
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey && _canContinue)
+        {
+            _canContinue = false;
+            FadeImage.DOFade(1, 0.65f).OnComplete(Continue);
+        }
+    }
+
+    private void Continue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator ShowStats()
+    {
+        CompletionTimeText.DOFade(1, 0.45f);
+
+        yield return new WaitForSeconds(0.35f);
+
+        BodiesSacrificedText.DOFade(1, 0.45f);
+
+        yield return new WaitForSeconds(0.85f);
+
+        ContinueText
+            .DOFade(1, 0.85f)
+            .SetLoops(-1, LoopType.Yoyo);
+
+        _canContinue = true;
+    }
+}
