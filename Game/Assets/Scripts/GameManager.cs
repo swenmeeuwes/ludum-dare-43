c78af2711555be093969286d1ea9479943a9c91b
Swen <swenmeeu@gmail.com>
2018-12-02 01:31:07 +0100

    Implemented goal

--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private PlayerManager _playerManager;
 
+    public PlayerManager PlayerManager {
+        get { return _playerManager; }
+    }
+
     private void Start()
     {
+        if (_playerManager == null)
+        {
+            _playerManager = FindObjectOfType<PlayerManager>();
+        }
+
         _playerManager.SpawnPlayer();
+    }
+
+    public void Win()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Game/Assets/Scripts/Goal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    [SerializeField]
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        if (_gameManager == null)
+        {
+            _gameManager = FindObjectOfType<GameManager>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            var player = collision.GetComponent<Player>();
+            if (player != null && _gameManager.PlayerManager.ActivePlayer == player)
+            {
+                _gameManager.Win();
+            }
+        }
+    }
+}
