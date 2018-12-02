a75b228f66cfca99285929f12bc1b5e5e0430d12
Swen <swenmeeu@gmail.com>
2018-12-02 01:30:09 +0100

    Implemented water

---
+++ Game/Assets/Scripts/Water.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (transform.position.y + transform.localScale.y / 2f > collision.transform.position.y + collision.transform.localScale.y / 2f)
+            {
+                var player = collision.GetComponent<Player>();
+                if (player != null && player.enabled)
+                {
+                    player.Kill();
+                }
+            }
+        }
+    }
+}
