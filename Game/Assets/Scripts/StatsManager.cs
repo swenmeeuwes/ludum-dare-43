c1f5532049fa8d7882143584eb740e34d44f1d81
Swen <swenmeeu@gmail.com>
2018-12-02 23:14:04 +0100

    Implemented stats manager

---
+++ Game/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class StatsManager : MonoBehaviour {
+    private static StatsManager _instance;
+    public static StatsManager Instance {
+        get {
+            return _instance;
+        }
+        set {
+            if (_instance == null)
+            {
+                _instance = value;
+            }
+        }
+    }
+
+    public int BodiesLost { get; set; }
+
+    private float _startTime;
+    public float StartTime { get { return _startTime; } }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        _startTime = Time.time;
+    }
+}
