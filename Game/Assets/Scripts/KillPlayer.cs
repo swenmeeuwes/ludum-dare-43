d35768326907cefb8f9ea3cf9d09b46d3d34eac7
Swen <swenmeeu@gmail.com>
2018-12-02 01:33:29 +0100

    Implemented kill player script

---
+++ Game/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class KillPlayer : MonoBehaviour {
+    private BoxCollider2D _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            var player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Kill();
+            }
+        }
+    }
+}
